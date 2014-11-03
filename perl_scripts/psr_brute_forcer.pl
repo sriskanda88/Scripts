934283370e8192adb498f298071c3df6e9775bf1
sriskanda88 <sriskanda88+git@gmail.com>
2014-11-02 16:09:20 -0800

    added stddev for gadget number

--- perl_scripts/psr_brute_forcer.pl
+++ perl_scripts/psr_brute_forcer.pl
@@ -2,6 +2,7 @@
 
 use Data::Dumper;
 use Math::Combinatorics;
+use Statistics::Basic qw(:all);
 use constant{GADGET=>"gadget", CLOBBER=>"clobber", STACK_MOD=>"stack_mod", RET_ADDR=>"ret_addr", FRAME_SIZE=>"frame_size", SUCCESS=>"success", TIME=>"time"};
 use constant{DEBUG=>(0), DUMP=>(0)};
 
@@ -38,6 +39,8 @@ sub main(){
     }
     close(FL);
     $total_gadgets = $gadget_num;
+    $total_gadgets = 5000;
+
     $time_for_all_gadgets = $total_gadgets * $time_per_gadget;
 
     #read all psr outs into the hash
@@ -57,6 +60,7 @@ sub main(){
     }
 
     print Dumper(%psr_hash) if DUMP;
+    print Dumper(%gadgets_hash) if DUMP;
 
     #start the analysis
     generate_permutations();
@@ -182,24 +186,35 @@ sub calculate_loop_times(){
 sub print_results(){
     my $best_combo;
     my $least_time_to_exploit = 0.0;
+    my $least_stddev = 0.0;
+
 
     print "Successful exploits :\n";
-    printf("%-20s\t%-10s\t%-10s\t%-10s\t%-10s\t%-10s\t%-10s\t%-10s\t%-10s\t%-10s\n", "Combination", "Reg 1", "Ret Addr", "Reg 2", "Ret Addr", "Reg 3", "Ret Addr", "Reg 4", "Ret Addr", "Time");
+    printf("%-20s\t%-16s\t%-5s\t%-16s\t%-5s\t%-16s\t%-5s\t%-16s\t%-5s\t%-20s\t%-5s\n", "Combination", "Reg 1", "Ret 1", "Reg 2", "Ret 2", "Reg 3", "Ret 3", "Reg 4", "Ret 4", "Time", "StdDev");
     foreach $combo_str (keys %exploit_hash){
         if ($exploit_hash{$combo_str}{SUCCESS} == 0){
             next;
         }
 
         my @regs = split(/ /, $combo_str);
-        printf("%-20s\t%-10s\t%-10s\t%-10s\t%-10s\t%-10s\t%-10s\t%-10s\t%-10s\t%-10s\n", $combo_str, $exploit_hash{$combo_str}{$regs[0]}{GADGET}, $exploit_hash{$combo_str}{$regs[0]}{RET_ADDR}, $exploit_hash{$combo_str}{$regs[1]}{GADGET}, $exploit_hash{$combo_str}{$regs[1]}{RET_ADDR}, $exploit_hash{$combo_str}{$regs[2]}{GADGET}, $exploit_hash{$combo_str}{$regs[2]}{RET_ADDR}, $exploit_hash{$combo_str}{$regs[3]}{GADGET}, $exploit_hash{$combo_str}{$regs[3]}{RET_ADDR}, $exploit_hash{$combo_str}{TIME});
+        my $vals = vector($gadgets_hash{$exploit_hash{$combo_str}{$regs[0]}{GADGET}}, $gadgets_hash{$exploit_hash{$combo_str}{$regs[1]}{GADGET}},$gadgets_hash{$exploit_hash{$combo_str}{$regs[2]}{GADGET}},$gadgets_hash{$exploit_hash{$combo_str}{$regs[3]}{GADGET}});
+        my $stddev = stddev($vals);
+
+        #printf("%-20s\t%-10s\t%-10s\t%-10s\t%-10s\t%-10s\t%-10s\t%-10s\t%-10s\t%-10s\n", $combo_str, $exploit_hash{$combo_str}{$regs[0]}{GADGET}, $exploit_hash{$combo_str}{$regs[0]}{RET_ADDR}, $exploit_hash{$combo_str}{$regs[1]}{GADGET}, $exploit_hash{$combo_str}{$regs[1]}{RET_ADDR}, $exploit_hash{$combo_str}{$regs[2]}{GADGET}, $exploit_hash{$combo_str}{$regs[2]}{RET_ADDR}, $exploit_hash{$combo_str}{$regs[3]}{GADGET}, $exploit_hash{$combo_str}{$regs[3]}{RET_ADDR}, $exploit_hash{$combo_str}{TIME});
+        printf("%-20s\t%-10s|%-6d\t%-5s\t%-10s|%-6d\t%-5s\t%-10s|%-6d\t%-5s\t%-10s|%-6d\t%-5s\t%-20s\t%-5f\n", $combo_str, $exploit_hash{$combo_str}{$regs[0]}{GADGET}, $gadgets_hash{$exploit_hash{$combo_str}{$regs[0]}{GADGET}}, $exploit_hash{$combo_str}{$regs[0]}{RET_ADDR}, $exploit_hash{$combo_str}{$regs[1]}{GADGET}, $gadgets_hash{$exploit_hash{$combo_str}{$regs[1]}{GADGET}}, $exploit_hash{$combo_str}{$regs[1]}{RET_ADDR}, $exploit_hash{$combo_str}{$regs[2]}{GADGET}, $gadgets_hash{$exploit_hash{$combo_str}{$regs[2]}{GADGET}}, $exploit_hash{$combo_str}{$regs[2]}{RET_ADDR}, $exploit_hash{$combo_str}{$regs[3]}{GADGET}, $gadgets_hash{$exploit_hash{$combo_str}{$regs[3]}{GADGET}}, $exploit_hash{$combo_str}{$regs[3]}{RET_ADDR}, $exploit_hash{$combo_str}{TIME}, $stddev);
 
         if ($least_time_to_exploit == 0.0 || $exploit_hash{$combo_str}{TIME} < $least_time_to_exploit){
             $least_time_to_exploit = $exploit_hash{$combo_str}{TIME};
             $best_combo = $combo_str;
         }
+
+        if (least_stddev == 0.0 || $stddev < $least_stddev){
+            $least_stddev = $stddev;
+        }
     }
 
     print "LEAST TIME TO EXPLOIT: $least_time_to_exploit for combination $best_combo\n";
+    print "LEAST STDDEV: $least_stddev\n";
 
 }
 
